d97a31e976d1c37f7cf43c844f672d0f87d86c94
Hope McGovern <hem52@cam.ac.uk>
2024-10-21 11:28:49 +0100

    cleaned up two SCons

--- new_scripts_IT/fetch_data.sh
+++
@@ -1,6 +0,0 @@
-#mkdir data
-# Septuagint Greek OT
-#curl -L https://sourceforge.net/projects/zefania-sharp/files/Bibles/GRC/The%20Analytic%20Septuagint/SF_2009-01-20_GRC_ALXX_%28THE%20ANALYTIC%20SEPTUAGINT%29.zip/download >> data/LXX.xml
-# Translator's Amalgamated Greek NT
-curl -L https://raw.githubusercontent.com/STEPBible/STEPBible-Data/refs/heads/master/Translators%20Amalgamated%20OT%2BNT/TAGNT%20Mat-Jhn%20-%20Translators%20Amalgamated%20Greek%20NT%20-%20STEPBible.org%20CC-BY.txt >> data/STEP/Mat_Jhn.txt
-curl -L https://raw.githubusercontent.com/STEPBible/STEPBible-Data/refs/heads/master/Translators%20Amalgamated%20OT%2BNT/TAGNT%20Act-Rev%20-%20Translators%20Amalgamated%20Greek%20NT%20-%20STEPBible.org%20CC-BY.txt >> data/STEP/Act_Rev.txt
